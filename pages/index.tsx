5630aa070b93cebc3816aa0d7a46958234e47d36
胡书康 <hsk287416@yahoo.co.jp>
2019-11-05 06:31:49 +0900

    首页开发完成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { NextPage, NextContext } from 'next';
 import * as commonActions from '../redux/action';
 import { request } from '../libs/github-api.lib';
@@ -9,15 +9,40 @@ import { Root, UserInfo, Avatar, Login, Name, Bio, Email, UserRepos } from '../s
 import { IUserInfo } from '../types/user-info.interface';
 import { Icon, Tabs } from 'antd';
 import RepoComp from '../components/Repo';
+import { withRouter, NextRouter } from 'next/router';
+import LRU from 'lru-cache';
 
 export interface IndexPage {
-    userRepos?: any;
-    userStarred?: any;
+    userRepos: any;
+    userStarred: any;
     userInfo: IUserInfo | null;
+    router: NextRouter;
 }
 
+const isServer = typeof window === 'undefined';
+const cache = new LRU({
+    maxAge: 1000 * 10
+})
+
 const IndexPage: NextPage<IndexPage> = (props: IndexPage) => {
-    const { userRepos, userStarred, userInfo } = props;
+    const { userRepos, userStarred, userInfo, router } = props;
+
+    const tabKey = router.query.key as string || '1';
+
+    const handleTabChange = useCallback((activeKey: string) => {
+        router.push(`/?key=${activeKey}`);
+    }, []);
+
+    useEffect(() => {
+        if (!isServer) {
+            if (userRepos) {
+                cache.set('userRepos', userRepos);
+            }
+            if (userStarred) {
+                cache.set('userStarred', userStarred);
+            }
+        }
+    }, [userRepos, userStarred]);
 
     if (userInfo) {
         return (
@@ -32,7 +57,7 @@ const IndexPage: NextPage<IndexPage> = (props: IndexPage) => {
                         <a href={`mailto:${userInfo.email}`}>{userInfo.email}</a>
                     </Email>
                 </UserInfo>
-                <Tabs defaultActiveKey="1" animated={false} style={{flexGrow: 1}}>
+                <Tabs activeKey={tabKey} animated={false} style={{ flexGrow: 1 }} onChange={handleTabChange}>
                     <Tabs.TabPane tab="我的仓库" key="1">
                         <UserRepos>
                             {
@@ -67,20 +92,26 @@ const IndexPage: NextPage<IndexPage> = (props: IndexPage) => {
 IndexPage.getInitialProps = async (ctx: NextContext) => {
     const { userInfo } = ctx.store.getState();
     const result: any = {};
-    if (userInfo) {
-        const userReposResp = await request({
-            method: 'GET',
-            data: {},
-            url: '/user/repos'
-        }, ctx.req);
-        result.userRepos = userReposResp.data;
 
-        const userStarredResp = await request({
-            method: 'GET',
-            data: {},
-            url: '/user/starred'
-        }, ctx.req);
-        result.userStarred = userStarredResp.data;
+    if (userInfo) {
+        result.userRepos = cache.get('userRepos');
+        result.userStarred = cache.get('userStarred');
+        if (!result.userRepos) {
+            const userReposResp = await request({
+                method: 'GET',
+                data: {},
+                url: '/user/repos'
+            }, ctx.req);
+            result.userRepos = userReposResp.data;
+        }
+        if (!result.userStarred) {
+            const userStarredResp = await request({
+                method: 'GET',
+                data: {},
+                url: '/user/starred'
+            }, ctx.req);
+            result.userStarred = userStarredResp.data;
+        }
     }
 
     return result;
@@ -92,4 +123,4 @@ const stateToProps = (state: CommonState) => {
     };
 }
 
-export default connect(stateToProps)(IndexPage);
+export default connect(stateToProps)(withRouter(IndexPage));
